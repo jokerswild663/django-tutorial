a189f29365fe3d40d4721656d73a471bb0360cb6
Jeff Wyckoff <jokerswild663@gmail.com>
2014-08-15 15:55:28 -0700

    modifying to use string instead of unicode

--- secondSite/polls/models.py
+++ secondSite/polls/models.py
@@ -6,7 +6,7 @@ class Poll(models.Model):
   question = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
 
-  def __unicode__(self):
+  def __str__(self):
     return self.question
 
 class Choice(models.Model):
@@ -14,5 +14,5 @@ class Choice(models.Model):
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
 
-  def __unicode__(self):
+  def __str__(self):
     return self.choice_text
