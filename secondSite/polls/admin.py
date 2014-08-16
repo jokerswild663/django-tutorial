0eeab6a0da01ad7adcb4dfdd99bcafda79c47671
Jeff Wyckoff <jokerswild663@gmail.com>
2014-08-15 17:30:48 -0700

    improving admin view

--- secondSite/polls/admin.py
+++ secondSite/polls/admin.py
@@ -7,10 +7,12 @@ class ChoiceInline(admin.TabularInline):
   extra = 1
 
 class PollAdmin(admin.ModelAdmin):
+  search_fields = ['question']
   fieldsets = [
     (None,               {'fields': ['question']}),
     ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
   ]
   inlines = [ChoiceInline]
+  list_display = ('question', 'pub_date', 'was_published_recently')
 
 admin.site.register(Poll, PollAdmin)
